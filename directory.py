e8042b371dff9d9710933cecc0f3b64215d887c5
Samuel-Hunter Berndt <berndts2@dev-intel16-k80.i>
2017-03-27 18:02:21 -0400

    create directory.py file, creates directory list of list

---
+++ directory.py
@@ -0,0 +1,6 @@
+# Temporary file to make directory structure
+# format: [['casia_mtcnn_cropped/CASIA_XXXXXXX/001.jpg', 'casia_mtcnn_cropped/CASIA_XXXXXX/002.jpg', ...], ...]
+
+import tensorflow as tf
+
+
