8ef8d624b968303cc6c144646acd3951021222f8
Grant Merlin King <kinggra1@dev-intel16-k80.i>
2017-03-27 21:20:34 -0400

    function-ified file list generation

--- directory.py
+++ directory.py
@@ -4,19 +4,22 @@
 import tensorflow as tf
 #tempFile = tf.gfile.GFile('~')
 
-image_files = []
 
-baseFileName = './casia_mtcnn_cropped/'
-#files = tf.gfile.Walk('./casia_mtcnn_cropped/CASIA_2426012')
-files = tf.gfile.Walk('./casia_mtcnn_cropped/')
-for file in files:
-#	print(file)
-	tempFile = []
-	for jpeg in file[2]:
-		tempFileName = file[0] + '/' + jpeg
-		#tempFileName = baseFileName + 'CASIA_2426012/' + jpeg 
-		tempFile.append(tempFileName)
-	image_files.append(tempFile)	
-print(image_files)
+get_cropped_CASIA_files():	
+	image_files = []
+
+	baseFileName = './casia_mtcnn_cropped/'
+	#files = tf.gfile.Walk('./casia_mtcnn_cropped/CASIA_2426012')
+	files = tf.gfile.Walk('./casia_mtcnn_cropped/')
+	for file in files:
+	#	print(file)
+		tempFile = []
+		for jpeg in file[2]:
+			tempFileName = file[0] + '/' + jpeg
+			#tempFileName = baseFileName + 'CASIA_2426012/' + jpeg 
+			tempFile.append(tempFileName)
+		image_files.append(tempFile)	
+	#print(image_files)
+	return image_files
 
 
